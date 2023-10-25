c24bc2b9efcbbe4b01c0b24dcac3f69bb8af5b2a
jayantced739 <109576199+jayantced@users.noreply.github.com>
2023-10-25 18:55:59 +0530

    added cart modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,20 +1,21 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = () => {
 
     const cartItems = [];
     return (
-      <div>
+      <Modal>
         {cartItems}
         <div className={classes.total}>
           <span>Total Amount</span>
           <span>35</span>
         </div>
-        <div className={classes.action}>
+        <div className={classes.actions}>
             <button className={classes['button--alt']}>Close</button>
             <button className={classes.button}>Order</button>
         </div>
-      </div>
+      </Modal>
     );
 }
 
